1972066f898305b1e5ae1b3f1e6f473a638f9bc6
kaizoku010 <dixontheworldvsy@gmail.com>
2025-10-25 02:26:11 +0300

    dev banner 0.1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,7 @@ function HomePage() {
 <TopContent/>
     </div>
     <VersionInfo 
-      version="0.1.0"
+      version="7.4.0"
       status="Under Development"
       lastUpdated="Oct 25, 2024"
     />
