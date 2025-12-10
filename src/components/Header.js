3755e3b2f44d20bfc2cb17e44dc48526beb72a18
kaizoku010 <dixontheworldvsy@gmail.com>
2025-12-10 22:10:47 +0300

    functions 05.

--- src/components/Header.js
+++ src/components/Header.js
@@ -78,7 +78,7 @@ function Header() {
             <Link to="/features">
               <button className='header_btn'>Features</button>
             </Link>
-            <button className='header_btn'>FAQs</button>
+            <button className='header_btn'>Faqs</button>
             <Link to="/sales">
               <button className='header_btn'>Contact</button>
             </Link>
